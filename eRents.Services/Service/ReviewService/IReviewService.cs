f6321557e88f79727bed23763bf8071ae6ea21b5
kenancosic <cosic.kenan@outlook.com>
2024-02-25 16:17:28 +0100

    Review CRUD

---
+++ eRents.Services/Service/ReviewService/IReviewService.cs
@@ -0,0 +1,18 @@
+﻿using eRents.Model.Requests;
+using eRents.Model.Response;
+using eRents.Model.SearchObjects;
+using eRents.Services.Database;
+using eRents.Services.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRents.Services.Service.ReviewService
+{
+    public interface IReviewService : ICRUDService<Review, ReviewSearchObject, ReviewsInsertRequest, ReviewsUpdateRequest>
+    {
+        ReviewsResponse GetReviewsByUsername(string username);
+    }
+}
